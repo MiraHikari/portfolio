be5664c5d0a89f9d8ea1c0483cbfd8f95ae3674a
MiraHikari <69497500+MiraHikari@users.noreply.github.com>
2024-10-05 17:30:05 +0800

    complete

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,11 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+  output: "hybrid",
+  adapter: cloudflare({ platformProxy: { enabled: true } })
 });
